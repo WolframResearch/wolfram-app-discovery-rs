b7b722847bd0e7dc32a4c988483fa01f1a63c3bd
Connor Gray <accounts@connorgray.com>
2022-01-14 16:47:09 -0600

    feature: Overhaul `wolfram-app-discovery` command-line executable
    Adds the following subcommands:
    
    	wolfram-app-discovery default [--app-type <type>]
    	wolfram-app-discovery list    [--app-type <type>]
    	wolfram-app-discovery inspect <APP_DIR>
    
    default:
    	Returns the "default" Wolfram application on this system.
    	This should be the newest and most full-featured instance
    	of the Wolfram System available.
    
    	(E.g. Mathematica > Wolfram Engine wrt. features)
    
    list:
    	List all installed Wolfram applications.
    
    inspect:
    	Print information about the specified Wolfram app.

--- src/bin/wolfram-app-discovery.rs
+++ src/bin/wolfram-app-discovery.rs
@@ -1,27 +1,125 @@
+use std::path::PathBuf;
+
 use clap::Parser;
 
-use wolfram_app_discovery::{self as wad, WolframApp, WolframAppType};
+use wolfram_app_discovery::{self as wad, Filter, WolframApp, WolframAppType};
 
 /// Discovery local installations of the Wolfram Language and Wolfram products.
 #[derive(Parser, Debug)]
 struct Args {
-    /// Wolfram products to include.
-    #[clap(long, arg_enum)]
-    product: Vec<WolframAppType>,
+    #[clap(subcommand)]
+    command: Command,
 }
 
-fn main() -> Result<(), wad::Error> {
-    let args: Args = Args::parse();
+#[derive(Parser, Debug)]
+enum Command {
+    /// Print the default Wolfram app.
+    ///
+    /// This method uses [`WolframApp::try_default()`] to locate the default app.
+    #[clap(display_order(1))]
+    Default(AppOpts),
+    /// List all locatable Wolfram apps.
+    #[clap(display_order(2))]
+    List(AppOpts),
+    /// Print information about a specified Wolfram application.
+    #[clap(display_order(3))]
+    Inspect {
+        app_dir: PathBuf,
 
-    let app = WolframApp::try_default()?;
-    let wl_version = app.wolfram_version()?;
+        #[clap(flatten)]
+        debug: Debug,
+    },
+}
+
+#[derive(Parser, Debug)]
+struct AppOpts {
+    /// Wolfram application types to include.
+    #[clap(long = "app-type", arg_enum)]
+    app_types: Vec<WolframAppType>,
+
+    #[clap(flatten)]
+    debug: Debug,
+}
+
+#[derive(Parser, Debug)]
+struct Debug {
+    /// Whether to print application information in the verbose Debug format.
+    #[clap(long)]
+    debug: bool,
+}
+
+//======================================
+// main()
+//======================================
+
+fn main() -> Result<(), wad::Error> {
+    let Args { command } = Args::parse();
+
+    match command {
+        Command::Default(opts) => default(opts),
+        Command::List(opts) => list(opts),
+        Command::Inspect { app_dir, debug } => inspect(app_dir, debug.debug),
+    }
+}
+
+//======================================
+// Subcommand entrypoints
+//======================================
+
+fn default(AppOpts { app_types, debug }: AppOpts) -> Result<(), wad::Error> {
+    let filter = make_filter(app_types);
+
+    let app = WolframApp::try_default_with_filter(&filter)?;
 
     println!("\nDefault Wolfram Language installation:\n");
 
-    println!("  Product:                     {:?}", app.app_type());
-    println!("  Wolfram Language version:    {}", wl_version);
-    #[rustfmt::skip]
-    println!("  $InstallationDirectory:      {}", app.installation_directory().display());
+    print_app_info(&app, debug.debug)?;
 
     Ok(())
+}
+
+fn list(AppOpts { app_types, debug }: AppOpts) -> Result<(), wad::Error> {
+    let filter = make_filter(app_types);
+
+    for (index, app) in wad::discover_with_filter(&filter).into_iter().enumerate() {
+        println!("\nWolfram App #{}:\n", index);
+        print_app_info(&app, debug.debug)?;
+    }
+
+    Ok(())
+}
+
+fn inspect(location: PathBuf, debug: bool) -> Result<(), wad::Error> {
+    let app = WolframApp::from_app_directory(location)?;
+
+    print_app_info(&app, debug)
+}
+
+//======================================
+// Utility functions
+//======================================
+
+fn print_app_info(app: &WolframApp, debug: bool) -> Result<(), wad::Error> {
+    if debug {
+        println!("{:#?}", app);
+    } else {
+        let wl_version = app.wolfram_version()?;
+
+        println!("  Product:                     {:?}", app.app_type());
+        println!("  Wolfram Language version:    {}", wl_version);
+        #[rustfmt::skip]
+        println!("  Application directory:       {}", app.app_directory().display());
+    }
+
+    Ok(())
+}
+
+fn make_filter(app_types: Vec<WolframAppType>) -> Filter {
+    let app_types = if app_types.is_empty() {
+        None
+    } else {
+        Some(app_types)
+    };
+
+    Filter { app_types }
 }

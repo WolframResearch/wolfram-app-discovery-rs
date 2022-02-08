6646248e757bccd8167ebd45956a7aab50bc39c2
Connor Gray <code@connorgray.com>
2022-02-08 14:11:46 -0600

    Fix use of compile_error!() in target_system_id() + expand supported Rust targets to all major modern platforms
    This was causing the docs.rs build of wolfram-app-discovery to fail.

--- build.rs
+++ build.rs
@@ -1,4 +1,9 @@
 fn main() {
+    println!(
+        "cargo:rustc-env=TARGET={}",
+        std::env::var("TARGET").unwrap()
+    );
+
     if std::env::var("TARGET").unwrap().contains("-apple") {
         println!("cargo:rustc-link-lib=framework=CoreServices");
     }
